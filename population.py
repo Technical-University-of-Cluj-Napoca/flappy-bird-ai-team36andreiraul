56fe8056c2836c2c61327633be890a9b7931d4e5
var166 <andreivarga166@gmail.com>
2025-12-15 11:43:03 +0200

    ai finished

--- population.py
+++ population.py
@@ -4,14 +4,13 @@ import math
 import species
 import operator
 
-
 class Population:
     def __init__(self, size):
         self.players = []
         self.generation = 1
         self.species = []
         self.size = size
-        for i in range(0, self.size):
+        for i in range(0,self.size):
             self.players.append(player.Player())
 
     def update_live_players(self):
@@ -19,32 +18,25 @@ class Population:
             if p.alive:
                 p.look()
                 p.think()
-                p.draw(config.window)
                 p.update(config.ground)
+                p.draw(config.window)
 
     def natural_selection(self):
-        print('SPECIATE')
+        print("SPECIATE")
         self.speciate()
-
-        print('CALCULATE FITNESS')
+        print("CALCULATE FITNESS")
         self.calculate_fitness()
-
         print('KILL EXTINCT')
         self.kill_extinct_species()
-
         print('KILL STALE')
         self.kill_stale_species()
-
-        print('SORT BY FITNESS')
+        print("SORT BY FITNESS")
         self.sort_species_by_fitness()
-
-        print('CHILDREN FOR NEXT GEN')
+        print("CHILDREN FOR NEXT GENERATION")
         self.next_gen()
-
     def speciate(self):
         for s in self.species:
             s.players = []
-
         for p in self.players:
             add_to_species = False
             for s in self.species:
@@ -54,13 +46,11 @@ class Population:
                     break
             if not add_to_species:
                 self.species.append(species.Species(p))
-
     def calculate_fitness(self):
         for p in self.players:
             p.calculate_fitness()
         for s in self.species:
             s.calculate_average_fitness()
-
     def kill_extinct_species(self):
         species_bin = []
         for s in self.species:
@@ -74,7 +64,7 @@ class Population:
         species_bin = []
         for s in self.species:
             if s.staleness >= 8:
-                if len(self.species) > len(species_bin) + 1:
+                if len(self.species) > len(species_bin)+1:
                     species_bin.append(s)
                     for p in s.players:
                         player_bin.append(p)
@@ -84,48 +74,29 @@ class Population:
             self.players.remove(p)
         for s in species_bin:
             self.species.remove(s)
-
     def sort_species_by_fitness(self):
         for s in self.species:
             s.sort_players_by_fitness()
-
         self.species.sort(key=operator.attrgetter('benchmark_fitness'), reverse=True)
-
     def next_gen(self):
         children = []
-
-        # Clone of champion is added to each species
+        #clone champion of each species
         for s in self.species:
             children.append(s.champion.clone())
-
-        # Fill open player slots with children
-        children_per_species = math.floor((self.size - len(self.species)) / len(self.species))
+        children_per_species = int(math.floor(self.size - len(self.species))/len(self.species))
         for s in self.species:
             for i in range(0, children_per_species):
                 children.append(s.offspring())
-
         while len(children) < self.size:
             children.append(self.species[0].offspring())
-
         self.players = []
         for child in children:
             self.players.append(child)
-        self.generation += 1
 
-    # Return true if all players are dead
     def extinct(self):
         extinct = True
         for p in self.players:
             if p.alive:
                 extinct = False
         return extinct
-
-
-
-
-
-
-
-
-
-
+    
